3865bea6c966b655435b9f527efa610ba844e444
carlos-norte <118981522+carlos-norte@users.noreply.github.com>
2023-11-05 19:20:24 -0100

    Add files via upload

---
+++ src/Estado.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace GoEat;
+
+class Estado extends Model
+{
+    protected ?int $id;
+    protected ?string $estado;
+
+    public function __construct($estado = ''){
+
+        parent::__construct('estados', 'id');
+
+        $this->id = null;
+        $this->estado = $estado;
+       
+    }
+
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of estado
+     */ 
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    /**
+     * Set the value of estado
+     *
+     * @return  self
+     */ 
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+}
